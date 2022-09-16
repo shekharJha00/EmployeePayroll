51150484ac94d90bc60dde6da0cf6b6e130ef704
shekhar <shekharjha7417870578@gmail.com>
2022-09-16 13:51:12 +0530

    ADDED uc4 : TO Print Employee Payroll Data

--- src/test/java/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Test;
 import java.util.Arrays;
 
@@ -10,8 +11,10 @@ public class EmployeePayrollServiceTest {
                 new EmployeePayrollData(3, "Nuts", 500000),
                 new EmployeePayrollData(4, "Naman", 800000),
         };
-        EmployeePayrollService employeePayrollService;
-        employeePayrollService = new EmployeePayrollService(Arrays.asList(payrollData));
+        EmployeePayrollService employeePayrollService= new EmployeePayrollService(Arrays.asList(payrollData));
         employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        int entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(4, entries);
     }
 }
