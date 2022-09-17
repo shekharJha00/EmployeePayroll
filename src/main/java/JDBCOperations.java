672b3cba76a79df9eca65141acc5b82f6255afcc
shekhar <shekharjha7417870578@gmail.com>
2022-09-18 00:36:12 +0530

    ADDED uc10 : RetriveData By Name in Employee_Payroll

--- src/main/java/JDBCOperations.java
+++ src/main/java/JDBCOperations.java
@@ -2,35 +2,64 @@ import java.sql.*;
 
 
 public class JDBCOperations {
-    public static void main(String[] args) {
+    static Connection con;
+    public static void main(String[] args) throws Exception {
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
 
-        Connection con = null;
-        Statement stmt = null;
-        String qry = "update employee_payroll set basic_pay=3000000.00 where name='Shekhar'";
+        String qry = "select * from employee_payroll where Emp_Name='Shekhar'";
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
             System.out.println("Driver Class Loaded");
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=1234");
+            con = DBConnection.createCP();
 
             System.out.println("Connection Establish with db server");
 
-            stmt = con.createStatement();
-            System.out.println("Platform Created");
-
-            stmt.executeUpdate(qry);
+            preparedStatement = con.prepareStatement(qry);
             System.out.println("Data Update");
+
+            rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                long phone_number = rs.getLong(3);
+                String address = rs.getString(4);
+                String department = rs.getString(5);
+                String gender = rs.getString(6);
+                double basic_pay = rs.getDouble(7);
+                double deductions = rs.getDouble(8);
+                double taxable_pay = rs.getDouble(9);
+                double tax = rs.getDouble(10);
+                double met_pay = rs.getDouble(11);
+                Date state = rs.getDate(12);
+
+                System.out.println("EmpId :-" + id + "  " + "EmpName :- " + name + " " + "Emp_PhoneNo. :-"
+                        + phone_number + " " + "EmpAddress :- " + address + " " + " EmpDepartment :-" + department + " "
+                        + "Gender :- " + gender + " " + "Basic_Pay " + basic_pay + " " + " Deduction :- " + deductions
+                        + " " + "Taxable_pay :- " + taxable_pay + " " + "Tax :-" + tax + " " + "Met_Pay :- " + met_pay
+                        + " " + "Date :-" + state);
+            }
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
 
         finally {
-            if (stmt != null) {
+            if (rs != null) {
                 try {
-                    stmt.close();
-                    System.out.println("Closed All Resources");
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
@@ -39,13 +68,15 @@ public class JDBCOperations {
             if (con != null) {
                 try {
                     con.close();
-                    System.out.println("Closed All Resources");
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
 
             }
+            System.out.println("Closed All Resources");
         }
+
     }
 }
+
 
